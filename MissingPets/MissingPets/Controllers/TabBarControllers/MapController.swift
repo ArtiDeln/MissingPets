a14dde8071481edf5b68effc7e56ac8ee368d158
Artyom Butorin <butorin.artiom@gmail.com>
2022-05-30 03:01:45 +0300

    Added vet clinics on the map

--- MissingPets/MissingPets/Controllers/TabBarControllers/MapController.swift
+++ MissingPets/MissingPets/Controllers/TabBarControllers/MapController.swift
@@ -9,29 +9,106 @@ import UIKit
 import MapKit
 import SnapKit
 
-class MapVC: UIViewController, MKMapViewDelegate {
+class MapVC: UIViewController {
 
+    //MARK: - GUI
+    
     private let mapView: MKMapView = {
             let mapView = MKMapView()
             mapView.translatesAutoresizingMaskIntoConstraints = false
             return mapView
         }()
 
+    //MARK: - Constants
+    
+    let locationManager = CLLocationManager()
+    
+    //MARK: - MapsMarks
+    
+    let modelVets = VetModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.tabBarController?.tabBar.backgroundColor = .systemBackground
+        
         self.navigationController?.navigationBar.prefersLargeTitles = true
-
         self.navigationItem.title = "Карта ветклиник"
+        
         self.view.addSubview(self.mapView)
         self.mapView.delegate = self
 
-        self.mapView.snp.makeConstraints {
-//            $0.edges.equalToSuperview()
-            $0.edges.equalTo(self.view)
+        self.constraints()
+        
+        for vet in modelVets.vets.first! {
+            mapView.addAnnotation(vet)
         }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            checkLocationEnabled()
+    }
+    
+    //MARK: - Constraints
+
+    private func constraints() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalTo(self.view)
+        }
+    }
+
+    //MARK: - Location
+    
+    func checkLocationEnabled() {
+            if CLLocationManager.locationServicesEnabled() {
+                setupManager()
+                checkAuthorization()
+            } else {
+                showAlertLocation(title: "Служба геолокации выключена", message: "Хотите включить?", url: URL(string: "App-Prefs:root=LOCATION_SERVICES"))
+            }
+        }
+
+        func setupManager() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+
+        func checkAuthorization() {
+            switch locationManager.authorizationStatus {
+            case .authorizedAlways:
+                break
+            case .authorizedWhenInUse:
+                mapView.showsUserLocation = true
+                locationManager.startUpdatingLocation()
+                break
+            case .denied:
+                showAlertLocation(title: "Вы запретили использование геолокации", message: "Хотите изменить?", url: URL(string: UIApplication.openSettingsURLString))
+                break
+            case .restricted:
+                break
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            @unknown default:
+                fatalError()
+            }
+        }
+
+        func showAlertLocation(title: String, message: String?, url: URL?) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let settingAction = UIAlertAction(title: "Настройки", style: .default) { (alert) in
+                if let url = url {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }
+            let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+
+            alert.addAction(settingAction)
+            alert.addAction(cancelAction)
+
+            present(alert, animated: true, completion: nil)
+        }
+
 }
 
 extension MapVC: CLLocationManagerDelegate {
@@ -42,5 +119,57 @@ extension MapVC: CLLocationManagerDelegate {
             mapView.setRegion(region, animated: true)
         }
     }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAuthorization()
+    }
 
+}
+
+extension MapVC: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Vets else { return nil }
+        var viewMarker: MKMarkerAnnotationView
+        let idView = "makrer"
+        if let view = mapView.dequeueReusableAnnotationView(withIdentifier: idView) as? MKMarkerAnnotationView {
+            view.annotation = annotation
+            viewMarker = view
+        } else {
+            viewMarker = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: idView)
+            viewMarker.canShowCallout = true
+            viewMarker.calloutOffset = CGPoint(x: 0, y: 0)
+            viewMarker.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return viewMarker
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        self.mapView.removeOverlays(mapView.overlays)
+        let vet = view.annotation as! Vets
+        let startPoint = MKPlacemark(coordinate: coordinate)
+        let endPoint = MKPlacemark(coordinate: vet.coordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startPoint)
+        request.destination = MKMapItem(placemark: endPoint)
+        request.transportType = .automobile
+        
+        let direction = MKDirections(request: request)
+        direction.calculate { (response, error) in
+            guard let response = response else { return }
+            for route in response.routes {
+                self.mapView.addOverlay(route.polyline)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay)
+        render.strokeColor = .blue
+        render.lineWidth = 4
+        
+        return render
+    }
 }
